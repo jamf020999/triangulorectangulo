596e8931b6806586ff8f8b91b0a148a59c2a77ed
jamf020999 <72583326+jamf020999@users.noreply.github.com>
2021-02-12 17:43:39 -0400

    Add files via upload

---
+++ ejecutar.php
@@ -0,0 +1,10 @@
+<?php
+$base = 3;
+$altura = 4;
+$area= null;
+$area = ((($base * $base) + ($altura * $altura))/5);
+echo 'Calculo del area del triangulo', '<br />';
+echo 'Base del Triangulo: ' . $base, '<br />';
+echo 'Altura del Triangulo: ' . $altura, '<br />';
+echo 'Area del Triangulo: ' . $area, '<br />';
+?>
